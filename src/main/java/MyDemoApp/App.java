0c8d4e6d9905be0099230c4b4acfbee784ec3af2
Abdulkadir Ozpolat <abdulkadirozpolatt@gmail.com>
2020-11-26 22:04:05 +0300

    created web app

--- src/main/java/MyDemoApp/App.java
+++ src/main/java/MyDemoApp/App.java
@@ -4,12 +4,71 @@
 package MyDemoApp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 public class App {
     public static void main(String[] args) {
-        
-       
+        get("/", (req, res) -> "Hello, World");
+        get("/compute",
+            (rq, rs) -> {
+              Map<String, String> map = new HashMap<String, String>();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine()
+        );
 
+        post("/compute", (req, res) -> {
+  
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> fromArray = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+              int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+              fromArray.add(value);
+            }
+            sc1.close();
+            String input2 = req.queryParams("input2");
+            sc1 = new java.util.Scanner(input2);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> toArray = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+              int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+              toArray.add(value);
+            }
+
+            sc1.close();
+  
+  
+            String input3 = req.queryParams("input3").replaceAll("\\s","");
+            int startindex = Integer.parseInt(input3);
+            String input4 = req.queryParams("input4").replaceAll("\\s","");
+            int finishindex = Integer.parseInt(input4);
+
+             Map<String, String> map = new HashMap<String, String>();
+            try{
+            ArrayList<Integer> result = App.copyArray(fromArray,toArray,startindex,finishindex);
+            map.put("result", " is " + result.toString());
+            }
+            catch(RuntimeException e){
+              String result= "could not be computed because of invalid inputs.Please try again with valid inputs."; 
+              map.put("result", result.toString());
+            }
+            
+            return new ModelAndView(map, "compute.mustache");
+          }, new MustacheTemplateEngine());
+  
+  
     }
 
 
